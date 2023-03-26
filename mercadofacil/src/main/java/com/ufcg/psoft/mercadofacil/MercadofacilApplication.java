50ddec1248df4e847d0063629cbd777d51b62a09
Ivyna Rufino <ivyna.rufino@nubank.com.br>
2023-03-26 19:45:02 -0300

    Prática 2 - Introdução ao Teste de Software

--- mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/MercadofacilApplication.java
+++ mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/MercadofacilApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.ufcg.psoft.mercadofacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MercadofacilApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MercadofacilApplication.class, args);
 	}
 
 }
--- mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/MercadofacilApplicationTests.java
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/MercadofacilApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.ufcg.psoft.mercadofacil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MercadofacilApplicationTests {
 
 	@Test
 	void contextLoads() {
